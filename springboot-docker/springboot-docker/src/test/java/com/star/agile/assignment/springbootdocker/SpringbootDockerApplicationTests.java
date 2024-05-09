4b7a0c4543e159c66d54f65ae910085d95f1ae8f
chandrabhan <chandrabhant98@gmail.com>
2024-05-10 02:18:17 +0530

    first commit

---
+++ springboot-docker/springboot-docker/src/test/java/com/star/agile/assignment/springbootdocker/SpringbootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.star.agile.assignment.springbootdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
